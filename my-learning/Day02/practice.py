45aed90a2b1cb412c304b60cdb97d175a78a457d
jj <shaojieliang1103@icloud.com>
2026-07-21 19:28:33 +0800

    practice

---
+++ my-learning/Day02/practice.py
@@ -0,0 +1,16 @@
+# print('hello, python')
+# print(2 + 3)
+# print(12*8)
+# print('my name is shaojie')
+# print('i am learning python')
+# print("Hello")
+# print(100)
+# print(3.14)
+# print(True)
+# name = "Shaojie"
+# age = 23
+
+# print("Name:",name)
+# print("Age:",age)
+print('a','b','c',sep='-')
+print('1','2','3',sep='***')
