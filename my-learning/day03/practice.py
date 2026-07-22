4d449b6fcccbe7fa3e7ef4b3b251db729b56c149
jj <shaojieliang1103@icloud.com>
2026-07-22 17:27:23 +0800

    practice

---
+++ my-learning/day03/practice.py
@@ -0,0 +1,20 @@
+# # # name='shaojie'
+# # # age=22
+# # # print(name)
+# # # print(age)
+# # name='shaojie'
+# # major='data science'
+# # study_day='day03'
+# # print(name,major,study_day, sep='-')
+# name='shaojie'
+# age=22
+# height=1.72
+# student=True
+# print(type(name),type(age),type(height),type(student),sep='\n')
+a='100'
+b=3.14
+c=250
+a=int(a)
+b=int(b)
+c=str(c)
+print(type(a),type(b),type(c),sep='\n')
