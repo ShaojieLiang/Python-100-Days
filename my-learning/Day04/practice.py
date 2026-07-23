ce81aa679d99e14db1f212c1d4a0607b0a1e8fc7
jj <shaojieliang1103@icloud.com>
2026-07-23 19:55:02 +0800

    Clean up Day04 practice exercises

--- my-learning/Day04/practice.py
+++ my-learning/Day04/practice.py
@@ -1,49 +1,64 @@
 """
 Day04 练习：Python 运算符
 
-使用方法：
-1. 按照课堂要求在对应区域写代码。
-2. 每完成一小题，运行一次文件检查结果。
-3. 不确定类型时，可以使用 type() 检查。
+本文件保存我在 Day04 亲自完成的练习代码。
 """
 
 
 # 1. 算术运算符练习
-# TODO: 定义两个数字变量，练习 +、-、*、/、//、%、**。
-# a=7
-# b=2
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-# print(a//b)
-# print(a%b)
-# print(a**b)
+# 练习 +、-、*、/、//、%、**。
+a = 7
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
 
 # 2. 赋值运算符练习
-# TODO: 定义一个变量 score，尝试使用 +=、-=、*= 修改它。
-# score=80
-# score+=10
-# print(score)
-# score-=20
-# print(score)
-# score*=2
-# print(score)
+# 使用 +=、-=、*= 修改 score。
+score = 80
+
+score += 10
+print(score)
+
+score -= 20
+print(score)
+
+score *= 2
+print(score)
+
 
 # 3. 比较运算符练习
-# TODO: 定义 age，判断它是否大于等于 18。
-# age=20
-# print(age >= 18,age < 18,age == 20,age != 20,sep='\n')
+# 判断 age 和 18、20 的关系。
+age = 20
+
+print(age >= 18)
+print(age < 18)
+print(age == 20)
+print(age != 20)
 
 
 # 4. 逻辑运算符练习
-# TODO: 定义两个 bool 变量，练习 and、or、not。
+# 练习 and、or、not。
+print(age >= 18 and age <= 60)
+print(age >= 18 or age > 60)
+print(not (age == 20))
 
 
 # 5. 综合练习
-# TODO: 根据课堂要求完成一个小程序。
-age= 22
-student=True
-print(age<=25 and student==True)
-if age<=25 and student==True:
-    print("你符合条件")
+# 规则：25 岁及以下的学生，或者 60 岁及以上的老人，可以享受优惠。
+age = 22
+student = True
+
+can_get_discount = (age <= 25 and student) or age >= 60
+print(can_get_discount)
+
+if can_get_discount:
+    print("你符合优惠条件")
+else:
+    print("你不符合优惠条件")
