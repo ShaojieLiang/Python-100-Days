ac2b97a177967332f9f34f51e652dd0e2e24ab45
jj <shaojieliang1103@icloud.com>
2026-07-23 19:45:27 +0800

    practice

--- my-learning/Day04/practice.py
+++ my-learning/Day04/practice.py
@@ -10,14 +10,30 @@ Day04 练习：Python 运算符
 
 # 1. 算术运算符练习
 # TODO: 定义两个数字变量，练习 +、-、*、/、//、%、**。
-
+# a=7
+# b=2
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
 
 # 2. 赋值运算符练习
 # TODO: 定义一个变量 score，尝试使用 +=、-=、*= 修改它。
-
+# score=80
+# score+=10
+# print(score)
+# score-=20
+# print(score)
+# score*=2
+# print(score)
 
 # 3. 比较运算符练习
 # TODO: 定义 age，判断它是否大于等于 18。
+# age=20
+# print(age >= 18,age < 18,age == 20,age != 20,sep='\n')
 
 
 # 4. 逻辑运算符练习
@@ -26,3 +42,8 @@ Day04 练习：Python 运算符
 
 # 5. 综合练习
 # TODO: 根据课堂要求完成一个小程序。
+age= 22
+student=True
+print(age<=25 and student==True)
+if age<=25 and student==True:
+    print("你符合条件")
