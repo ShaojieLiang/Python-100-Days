39e878fcdda41f5b5468c110fc088accff7c5990
jj <shaojieliang1103@icloud.com>
2026-07-26 16:54:47 +0800

    practice

---
+++ my-learning/Day06/practice.py
@@ -0,0 +1,24 @@
+# for i in range(1, 6):
+#     print(i)
+# for i in range(1, 10, 3):
+#     print(i)
+total=0
+for i in range(1, 10):
+    total+=i
+print(total)
+total = 0
+
+for i in range(1, 101):
+    total += i
+
+print(total)
+total = 0
+for i in range(1, 101, 2):
+    total += i
+print(total)
+total = 0
+
+for i in range(2, 101, 2):
+    total += i
+
+print(total)
